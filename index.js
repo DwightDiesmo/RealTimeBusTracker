1843a68907a7e2822bc6d5760acbdef30d4279f6
DwightDiesmo <deightdiesmo@gmail.com>
2022-09-10 20:47:04 -0700

    using class

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-  [-117.1541, 32.7053],
-  [-117.1599, 32.7114],
-  [-117.1601, 32.7055],
-  [-117.1709, 32.7091],
-  [-117.1695, 32.7169],
-  [-117.170174, 32.722480],
-  [-117.174438, 32.733125],
-  [-117.16074, 32.749698],
-  [-117.1970, 32.7542],
-  [-117.1968, 32.7636],
-  [-117.1667, 32.7681],
-  [-117.1585, 32.7709],
-  [-117.1624, 32.7671],
-  [-117.1421, 32.7737],
-  [-117.128752, 32.781642],
-  [-117.1196, 32.7831],
-  [-117.1105, 32.7816],
-  [-117.0972, 32.7804],
-  [-117.0714, 32.7774],
-  [-117.0573, 32.7765],
-  [-117.0426, 32.7724],
-  [-117.0147, 32.7794],
-  [-117.0018, 32.7855],
-  [-116.9761, 32.7920],
-  [-116.97558, 32.80461],
-  [-116.9745, 32.8255],
-  [-116.9809, 32.8410],
+let accessToken = 'USE-YOUR-ACCESS-TOKEN';
+
+class trolleyStation {
+  constructor(name, lng, lat) {
+    this.name = name;
+    this.lng = lng;
+    this.lat = lat;
+    this.lgnlat = [this.lng, this.lat];
+  }
+}
+
+const sdGreenLine = [
+  new trolleyStation("12th & Imperial", -117.1541, 32.7053),
+  new trolleyStation("Gaslamp Quarter", -117.1599, 32.7114),
+  new trolleyStation("Convention Center", -117.1601, 32.7055),
+  new trolleyStation("Seaport Village", -117.1709, 32.7091),
+  new trolleyStation("Santa Fe Depot", -117.1695, 32.7169),
+  new trolleyStation("Little Italy", -117.170174, 32.722480),
+  new trolleyStation("Middletown", -117.174438, 32.733125),
+  new trolleyStation("Washington Street", -117.16074, 32.749698),
+  new trolleyStation("Old Town", -117.1970, 32.7542),
+  new trolleyStation("Morena Linda Vista", -117.1968, 32.7636),
+  new trolleyStation("Fashion Valley", -117.1667, 32.7681),
+  new trolleyStation("Hazard Center", -117.1585, 32.7709),
+  new trolleyStation("Mission Valley Center", -117.1624, 32.7671),
+  new trolleyStation("Rio Vista", -117.1421, 32.7737),
+  new trolleyStation("Fenton Parkway", -117.128752, 32.781642),
+  new trolleyStation("Stadium", -117.1196, 32.7831),
+  new trolleyStation("Mission San Diego", -117.1105, 32.7816),
+  new trolleyStation("Grantville", -117.0972, 32.7804),
+  new trolleyStation("SDSU", -117.0714, 32.7774),
+  new trolleyStation("Alvarado", -117.0573, 32.7765),
+  new trolleyStation("70th Street", -117.0426, 32.7724),
+  new trolleyStation("Grossmont", -117.0147, 32.7794),
+  new trolleyStation("Amaya Drive", -117.0018, 32.7855),
+  new trolleyStation("El Cajon", -116.9761, 32.7920),
+  new trolleyStation("Arnele Avenue", -116.97558, 32.80461),
+  new trolleyStation("Gillespie Field", -116.9745, 32.8255),
+  new trolleyStation("Santee", -116.9809, 32.8410),
 ];
 
 // TODO: add your own access token
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZHdpZ2h0ZGllc21vIiwiYSI6ImNsN3cwbndhNTBncWczbm1zYjBjOW0zc3kifQ.Rt6lA0T1uZIKZ1YulLyYYg";
+mapboxgl.accessToken = accessToken;
 
 // This is the map instance
 let map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [-117.0714, 32.7774],
+  center: [-117.1214, 32.7774],
   zoom: 11,
 });
 
 // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
 let marker = new mapboxgl.Marker()
-  .setLngLat([busStops[0][0], busStops[0][1]])
+  .setLngLat([sdGreenLine[0].lng, sdGreenLine[0].lat])
   .addTo(map);
 
 // counter here represents the index of the current bus stop
@@ -53,8 +62,8 @@ function move() {
   // Use counter to access bus stops in the array busStops
   // Make sure you call move() after you increment the counter.
   setTimeout(() => {
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
+    if (counter >= sdGreenLine.length) return;
+    marker.setLngLat(sdGreenLine[counter].lgnlat);
     counter++;
     move();
   }, 1000);
